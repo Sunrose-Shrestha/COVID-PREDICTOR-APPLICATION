c90e1213388c7d18dbad85eabc162ebe6539acfb
shres <shres@Sunrose>
2021-06-10 16:25:23 +0530

    new commit 10/06/21

--- HelloFX/src/application/Scene2Controller.java
+++ HelloFX/src/application/Scene2Controller.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -18,8 +20,45 @@ public class Scene2Controller {
 	private Button logoutButton;
 	@FXML
 	private AnchorPane scenePane;
+	@FXML
+	private Label yourResult;
+	@FXML
+	private RadioButton oldAge, semiOldAge, adults, teenage, healthWorker, policeMen, deliveryMen, shopKeeper;
+	@FXML
+	private static int points;
 	
 	Stage stage;
+	
+	public void getAge(ActionEvent event) {
+		if(oldAge.isSelected()) {
+			points+=20;
+		}
+		else if(semiOldAge.isSelected()) {
+			points+=15;
+		}
+		if(adults.isSelected()) {
+			points+=10;
+		}
+		if(teenage.isSelected()) {
+			points+=5;
+		}
+	}
+	
+	public void getProfession(ActionEvent event) {
+		points = 0;
+		if(healthWorker.isSelected()) {
+			points+=15;
+		}
+		else if(policeMen.isSelected()) {
+			points+=12;
+		}
+		if(deliveryMen.isSelected()) {
+			points+=10;
+		}
+		if(shopKeeper.isSelected()) {
+			points+=7;
+		}
+	}
 	
 	public void displayName(String username) {
 		nameLabel.setText("Hello! " + username + "\nWelcome to covid analysis app...");
@@ -37,4 +76,17 @@ public class Scene2Controller {
 			stage.close();
 		}
 	}
+	
+	public void displayResult(ActionEvent event) {
+		if(points==35) {
+			yourResult.setText("You have high chance of getting infected!!");
+		}
+		else if(points<35 && points>=20) {
+			yourResult.setText("You have average chance of getting infected!!");
+		}
+		else if(points<20) {
+			yourResult.setText("You have less chance of getting infected!!");
+		}
+	}
+	
 }
